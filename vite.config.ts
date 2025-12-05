c86d2d6037ada47278119c7f2426dcd37bd7a630
Andhika Haqqa Syahrony <bban97550@gmail.com>
2025-12-05 23:42:06 +0700

    vite build config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     build: {
+      // added some build output changes
       outDir:'/home/motodivs/admin.motodiv.store/',
         emptyOutDir: true,
     }
