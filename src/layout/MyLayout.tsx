3c5a176851c4aa2817b078d96181b9a64df75d3e
Andhika Haqqa Syahrony <bban97550@gmail.com>
2025-12-05 22:23:25 +0700

    Revert "oops"
    This reverts commit 218cf56b3cea2d854f0a201fd2ead96b44c272d7.

--- src/layout/MyLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { Layout} from 'react-admin';
-import type {LayoutProps}  from 'react-admin';
-import { MyMenu } from './MyMenu';
-
-export const MyLayout = (props: LayoutProps) => (
-    <Layout {...props} menu={MyMenu} />
-);
